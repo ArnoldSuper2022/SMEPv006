9fa7fa3984c7dbbf13fb10f77529777f6600c7e8
Elias Halel Arnold Copa Humerez <102313427+ArnoldSuper2022@users.noreply.github.com>
2022-07-22 15:38:02 -0400

    Update MainActivity.java
    CAMBIO SERVER

--- app/src/main/java/com/example/smepv006/MainActivity.java
+++ app/src/main/java/com/example/smepv006/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //MODIFICAMOS EN EL SERVER ELIAS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
