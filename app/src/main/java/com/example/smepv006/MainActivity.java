895fd7b1d5a344a502a39cdc256c9489710ee902
ArnoldSuper2022 <arnoldsuper2022@gmail.com>
2022-07-22 15:39:21 -0400

    prueba 003

--- app/src/main/java/com/example/smepv006/MainActivity.java
+++ app/src/main/java/com/example/smepv006/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //MODIFICAMOS EN EL SERVER ELIAS
+    //BUENO AQUI TE RESPONDO ARNOLD
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
